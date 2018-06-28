13019026461fe984ca41290ebe6fe371789a7fbd
Vicky <jie.bao1989@gmail.com>
2018-06-28 01:15:30 -0400

    script update for zombie in matrix

--- Algorithm/Zombie in Matrix.py
+++ Algorithm/Zombie in Matrix.py
@@ -18,8 +18,8 @@ class Solution:
     @return: an integer
     """
 
-    ## 类BFS的做法，对于grid，对人也就是0进行搜索， 找到人就设为1
-    ## 每次遍历完整个grid算是一次
+    ## 类分层BFS的做法，对于grid，对人也就是0进行搜索， 找到人就设为1
+    ## 每次遍历完整个grid算是一层搜索完成
     ## 设people_cnt and become_zombie_cnt,如果become_zombie_cnt = 0 return -1
     ## 当people_cnt == become_zombie_cnt, return 遍历个数
     def zombie(self, grid):
@@ -33,22 +33,24 @@ class Solution:
 
         ## initialize variables
         day_cnt = 1
-        peopleMetCnt = 0
-        becomeZombieCnt = 0
         delta_x = [0, 0, -1, 1]
         delta_y = [1, -1, 0, 0]
 
         while True:
+            peopleMetCnt = 0
+            becomeZombieCnt = 0
+            becomeZombieSet = set()  ## set来存储变成zombie的点坐标
             for i in range(row):
                 for j in range(col):
                     ##找到人了，然后查左右邻居，只要有一个是1就return并且变为1
                     if grid[i][j] == 0:
                         peopleMetCnt += 1
                         ## check neighbors for those people
-                        if self.neighborIsZombie(grid, i, j, delta_x, delta_y):
+                        if self.neighborIsZombie(grid, i, j, delta_x, delta_y, becomeZombieSet):
                             grid[i][j] = 1  ## 如果发现四周有zombie,就把人变为zombie
+                            becomeZombieSet.add((i, j))
                             becomeZombieCnt += 1
-
+            # print 'people count is %d, become zombie count is %d' %(peopleMetCnt, becomeZombieCnt)
             if peopleMetCnt != 0 and becomeZombieCnt == 0:
                 return -1
             if peopleMetCnt == becomeZombieCnt:
@@ -57,16 +59,18 @@ class Solution:
 
             ## detect people neighbors zombie or not and whether it's within boundary
 
-    def neighborIsZombie(self, grid, x, y, delta_x, delta_y):
+    def neighborIsZombie(self, grid, x, y, delta_x, delta_y, becomeZombieSet):
         for i in range(len(delta_x)):
             newX = x + delta_x[i]
             newY = y + delta_y[i]
 
-            if self.isBoundary(grid, newX, newY) and grid[newX][newY] == 1:
+            if self.isBoundary(grid, newX, newY) and grid[newX][newY] == 1 and (newX, newY) not in becomeZombieSet:
+                # print "yes"
                 return True
 
     ## boundary 判断
     def isBoundary(self, grid, x, y):
-        return x < len(grid) and y < len(grid[0])
+        return x >= 0 and x < len(grid) and y >= 0 and y < len(grid[0])
+
 
 
